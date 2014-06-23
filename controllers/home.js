d3ca83f9974285b19152c74593103447399946c7
alvin <alvinjosephvaldez@yahoo.com>
2014-06-23 14:07:32 +0800

    added a call socket.io call in html file

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,8 @@
 exports.index = function(req, res){
-        res.send("working");
+    res.send("working");
 };
 
 exports.something = function(req, res){
 
-        
-
-        res.render('home', { title: 'Express' , data: 'tang ina gumana!!'});
+    res.render('home', { title: 'Express' , data: 'tang ina gumana!!'});
 };
