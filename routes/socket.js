3c3520f26c739b6d95aa80370675bb0362f9f43d
alvin <alvinjosephvaldez@yahoo.com>
2014-07-06 21:33:22 +0800

    using socket id for private messaging

--- routes/socket.js
+++ routes/socket.js
@@ -2,41 +2,49 @@ var io = require('socket.io');
 
 exports.initialize = function(server){
     io = io.listen(server);
-    var usernames = [];
+    var usernames = {};
 
     io.sockets.on('connection', function(socket){
-        //receiver of message 
-        // socket.on('message', function(message){
-
-        //     message = JSON.parse(message);
-
-        //    if(message.type == "userMessage"){
-        //         socket.broadcast.send(JSON.stringify(message));
-        //         message.type = "myMessage";
-        //         socket.send(JSON.stringify(message));
-        //     }
-        // });
-
         socket.on('new user', function(data, callback){
-            if(usernames.indexOf(data) != -1){
+            if(data in usernames){
                 callback(false);
             }else{
                 callback(true);
                 //save the username to the socket for retrieval
                 socket.username = data;
-                usernames.push(socket.username);
-                io.sockets.emit('usernames', usernames);
+                usernames[socket.username] = socket.id;
+                io.sockets.emit('usernames', Object.keys(usernames));
             }
         });
 
-        socket.on('sendMessage', function(data){
-            //retrieve the username saved in the socket
-            io.sockets.emit('new message', {message: data, username: socket.username})
+        socket.on('sendMessage', function(data, callback){
+            var msg = data.trim();
+            if(msg.substr(0,3) === "/w "){
+                msg = msg.substr(3);
+                var space = msg.indexOf(" ");
+                if( space !== 1){
+                    var name = msg.substr(0, space);
+                    msg = msg.substr(space+1);
+                    if(name in usernames){
+                        // to send the message to specific socket id of users                         
+                        io.sockets.connected[usernames[name]].emit('new message', {message: msg, username: socket.username});
+                    }   
+                } else{ 
+                    callback("error invalid user");
+                }
+            } else{
+                //retrieve the username saved in the socket
+                io.sockets.emit('new message', {message: data, username: socket.username});
+            }
+
         });
 
         socket.on('disconnect', function(data){
             if(!socket.username) return;
-            usernames.splice(usernames.indexOf(socket.username), 1);
+            //NEW
+            delete usernames[socket.nickname];
+            // OLD
+            //usernames.splice(usernames.indexOf(socket.username), 1);
             io.sockets.emit('usernames', usernames);
         });
     });
